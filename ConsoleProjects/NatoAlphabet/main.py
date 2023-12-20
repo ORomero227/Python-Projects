82e7bf3cd730a3e90842bb794f12d7dc4702de6c
Oscar Romero <oscalito992@gmail.com>
2023-12-20 19:45:55 -0400

    NatoAlphabet Project

--- ConsoleProjects/NatoAlphabet/main.py
+++ ConsoleProjects/NatoAlphabet/main.py
@@ -2,7 +2,6 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# TODO 1: Create a dictionary in this format {"A":"Alfa" ...}
 alphabet = {rows.letter: rows.code for (index, rows) in data.iterrows()}
 
 user_word = input("Enter a single word ex.House: ")
